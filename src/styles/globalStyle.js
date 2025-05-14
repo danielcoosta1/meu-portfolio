87f25a05aa3cbb49091f84513574df5630b0ab45
Daniel Costa <daniel.dc697@gmail.com>
2025-05-14 14:25:46 -0300

    Aqui, vamos ajustar para que o layout inteiro (inclusive a sidebar) seja centralizado na tela.

--- src/styles/globalStyle.js
+++ src/styles/globalStyle.js
@@ -1,18 +1,27 @@
+// src/styles/global.js
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
-  * {
+  html, body {
     margin: 0;
     padding: 0;
+    height: 100%;
+    overflow: hidden;  /* Impede que o scroll afete o body */
+    font-family: 'Inter', sans-serif;
+    display: flex;
+    justify-content: center; /* Centraliza o layout na tela */
+  }
+  
+  * {
     box-sizing: border-box;
   }
 
   body {
-    font-family: 'Inter', sans-serif;
     background-color: #0a192f;
     color: #fff;
+    width: 100%;
   }
-
+  
   a {
     text-decoration: none;
     color: inherit;
