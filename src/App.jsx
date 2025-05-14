68dccf0744de2f172020f6f35ddbc6184964f4d4
Daniel Costa <daniel.dc697@gmail.com>
2025-05-14 15:35:29 -0300

    Colocando um teste para ver a estilização

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import Teste from "./components/ComponenteTeste/Teste";
 import Layout from "./components/Layout";
 
 
 function App() {
   return (
     <Layout>
-      
+      <Teste/>
     </Layout>
   );
 }
