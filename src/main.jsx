a29a9bc6b3b591b7ef24480e1e025327c0483ff8
Daniel Costa <daniel.dc697@gmail.com>
2025-05-14 15:35:17 -0300

    importando novamente o App

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { GlobalStyle } from "./styles/globalStyle.js";
 
 import App from "./App.jsx";
+import { GlobalStyle } from "./styles/globalStyle.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
