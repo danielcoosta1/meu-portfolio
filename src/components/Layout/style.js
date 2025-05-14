c4a46ad373e582eeb3c323b35a625b0eb8413192
Daniel Costa <daniel.dc697@gmail.com>
2025-05-14 13:49:23 -0300

    mudando o position para sticky no layout

--- src/components/Layout/style.js
+++ src/components/Layout/style.js
@@ -1,16 +1,27 @@
-import styled from 'styled-components';
+// src/components/Layout/styles.js
+import styled from "styled-components";
 
-export const Container = styled.div`
-  display: flex;
+export const SidebarContainer = styled.aside`
+  width: 550px;
   height: 100vh;
-  width: 100vw;
-  overflow: hidden;
+  padding: 2rem;
+  background-color: #0a192f;
+  color: #ccd6f6;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  position: sticky;
+  top: 0;
+  align-self: flex-start;
+  box-sizing: border-box;
 `;
 
 export const Content = styled.main`
   flex: 1;
-  height: 100vh;
+  height: 100%;
   overflow-y: auto;
   scroll-behavior: smooth;
-  padding: 2rem; // ajustável
+  padding-right: 1rem; /* Espaço interno à direita */
+  max-width: 100%; /* Garante que o Content respeite o máximo do container */
+  width: 100%; /* Garante que o Content ocupe toda a largura disponível */
 `;
