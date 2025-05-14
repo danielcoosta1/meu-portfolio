b41423c589be8a04433a0685f87323983a22df81
Daniel Costa <daniel.dc697@gmail.com>
2025-05-14 12:51:05 -0300

    os componentes em react precisam começar com a letra maiuscula

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { globalStyle } from "./styles/globalStyle.js";
+import { GlobalStyle } from "./styles/globalStyle.js";
 
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <globalStyle/>
+    <GlobalStyle/>
     <App />
   </StrictMode>
 );
