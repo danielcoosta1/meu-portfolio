8ddf458068993becc1abfe56560b0c5e77a82014
Daniel Costa <daniel.dc697@gmail.com>
2025-05-15 16:03:03 -0300

    Ajustando o padding do conteudo(content do layout)

--- src/components/Layout/style.js
+++ src/components/Layout/style.js
@@ -7,7 +7,7 @@ export const Container = styled.div`
   max-width: 1400px;
   margin: 0 auto;
   padding: 2rem;
-  padding-left: 550px; // Sidebar (350px) + respiro
+  padding-left: 480px; // Sidebar (350px) + respiro
   box-sizing: border-box;
   position: relative;
   gap: 2rem;
@@ -29,12 +29,12 @@ export const ContentWrapper = styled.div`
 export const Content = styled.main`
   width: 100%;
   max-width: 1200px;
-  padding: 1.25rem;
+  padding: 2rem 0;
   box-sizing: border-box;
 
 
   @media (max-width: 1200px){
-    padding:0;
+    padding:2rem;
   }
 
 
