fe06a1702f27747f60f280e9f5689d7c8eb2afa5
Daniel Costa <daniel.dc697@gmail.com>
2025-05-19 09:24:56 -0300

    Criando a pagina que irá receber a lista de projetos

---
+++ src/pages/Projetos/styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.section`
+  max-width: 960px;
+  margin: 0 auto;
+  padding: 4rem 2rem;
+`;
+
+export const Titulo = styled.h2`
+  font-size: 2.5rem;
+  margin-bottom: 2rem;
+  color: #e2e8f0;
+`;
